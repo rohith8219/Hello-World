7bea34a989837683438597512c872c2786be5472
rohith8219 <rr8219@srmist.edu.in>
2026-03-25 14:00:38 +0530

    uc4-display-Hello

--- HelloWorld.java
+++ HelloWorld.java
@@ -1,10 +1,30 @@
-public class HelloWorld{
- public static void main(String args[]){
-	 String name ="World";
-	 if (args.length >0){
-		 name = args[0];
-	 }
-	 System.out.println(" Hello, " + name + "!");
-	 }
-	 }
-}	 
+public class HelloApp {
+
+    public static void main(String[] args) {
+
+        String name;
+
+        // Check if arguments are provided
+        if (args.length > 0) {
+            StringBuilder nameBuilder = new StringBuilder();
+
+            // Loop through all arguments
+            for (int i = 0; i < args.length; i++) {
+                nameBuilder.append(args[i]);
+
+                // Add comma if not the last element
+                if (i < args.length - 1) {
+                    nameBuilder.append(", ");
+                }
+            }
+
+            name = nameBuilder.toString();
+        } else {
+            // Default value if no arguments
+            name = "World";
+        }
+
+        // Print the greeting
+        System.out.println("Hello " + name);
+    }
+}
